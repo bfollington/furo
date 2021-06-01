0b877a12ba63b70c3c6724eecc1ed2f4ad3acef4
Ben Follington <5009316+bfollington@users.noreply.github.com>
2021-06-01 10:02:54 +1000

    Format index.tsx

--- packages/example/src/index.tsx
+++ packages/example/src/index.tsx
@@ -1,14 +1,14 @@
-import { createBus } from "@twopm/furo";
-import { HistoryDebugger } from "@twopm/furo/lib/HistoryDebugger";
-import React, { StrictMode } from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./index.css";
-import { Message, MessageContext } from "./messages";
-import reportWebVitals from "./reportWebVitals";
+import { createBus } from '@twopm/furo'
+import { HistoryDebugger } from '@twopm/furo/lib/HistoryDebugger'
+import React, { StrictMode } from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './index.css'
+import { Message, MessageContext } from './messages'
+import reportWebVitals from './reportWebVitals'
 
 // Cofigure whether debugging is enabled
-const bus = createBus<Message>(true);
+const bus = createBus<Message>(true)
 
 ReactDOM.render(
   <StrictMode>
@@ -18,10 +18,10 @@ ReactDOM.render(
       <HistoryDebugger context={MessageContext} />
     </MessageContext.Provider>
   </StrictMode>,
-  document.getElementById("root")
-);
+  document.getElementById('root')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
